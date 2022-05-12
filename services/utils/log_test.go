5a9e0ad85c272c6b5612081f6cf0a9a7472d5145
Yaroslav Fedchenko <yarikf02@gmail.com>
2022-05-12 18:25:04 +0300

    add utils

--- services/utils/log_test.go
+++ services/utils/log_test.go
@@ -1,3 +1,19 @@
 package log_test
 
-import ""
+import (
+	"context"
+	"testing"
+
+	"github.com/Yarikf01/graduatedwork/services/utils/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogWithContext(t *testing.T) {
+	ctx := context.Background()
+	assert.Equal(t, log.L, log.FromContext(ctx))
+
+	newLog := log.L.With("field-1", 12)
+	ctx = log.WithLogger(ctx, newLog)
+
+	assert.Equal(t, newLog, log.FromContext(ctx))
+}
